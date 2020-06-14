3bd5f09a023ef19df3b9fc938e8b35236489c71c
Dmitry Kisler <admin@dkisler.com>
2020-06-14 13:20:29 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# conntectors-framework
-Framework to interface various data sources
+# Conntectors Framework
+
+[![license](https://img.shields.io/pypi/l/ansicolortags.svg)](./LICENSE)
+[![pyversion](https://img.shields.io/static/v1?label=python&color=blue&message=3.7%20|%203.8)](./)
+
+Framework to interface various services of public cloud providers. 
+
+## Objective
+
+The framework aims to provide an abstraction layer with unified methods to communicate with *cloud storage*, *databases* and other services regardless of what public cloud provider is being used.
+
+## Package structure
+
+```bash
+cloud_connectors
+├── cloud_storage
+├── database
+├── tests
+│   ├── cloud_storage
+│   └── database
+└── utils
+```
---
+++ setup.py
@@ -0,0 +1,35 @@
+# Dmitry Kisler © 2020-present
+# www.dkisler.com
+
+import pathlib
+from setuptools import setup, find_namespace_packages
+
+
+DIR = pathlib.Path(__file__).parent
+requirements = (DIR / "requirements.txt").read_text()
+README = (DIR / "README.md").read_text()
+
+PKG_NAME = "cloud-connectors"
+
+setup(
+    name=PKG_NAME,
+    version='1.0.0',
+    description="Framework to abstract public cloud services.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/kislerdm/{PKG_NAME}",
+    author="Dmitry Kisler",
+    author_email="admin@dkisler.com",
+    license='MIT',
+    classifiers=[
+        "Development Status :: 2 - Alpha",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_namespace_packages(where='.',
+                                     exclude=('tests',)),
+    install_requires=requirements,
+    include_package_data=True,
+)
