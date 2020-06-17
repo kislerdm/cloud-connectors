958dec25414f88e689954fd3efe415238d309593
Dmitry Kisler <admin@dkisler.com>
2020-06-17 22:55:11 +0200

    adds some gcp methods

--- cloud_connectors/cloud_storage/gcs.py
+++ cloud_connectors/cloud_storage/gcs.py
@@ -3,12 +3,13 @@
 
 import os
 from typing import Union, List, Tuple
+import time
 from google.cloud import storage
-# from cloud_connectors.cloud_storage.common import Client as ClientCommon
-# import cloud_connectors.cloud_storage.exceptions
+from cloud_connectors.cloud_storage.common import Client as ClientCommon
+from cloud_connectors.cloud_storage import exceptions
 import fastjsonschema
-from common import Client as ClientCommon
-import exceptions
+# from common import Client as ClientCommon
+# import exceptions
 
 
 class Client(ClientCommon):
@@ -24,16 +25,138 @@ class Client(ClientCommon):
 
     Raises:
       ConnectionError: Raised when a connection error to s3 occurred.
-      cloud_connectors.cloud_storage.exceptions.ConfigurationError: Raised when provided connection configuration is wrong.
+      cloud_connectors.cloud_storage.exceptions.ConfigurationError: 
+        Raised when provided connection configuration is wrong.
     """
+    CLIENT_CONFIG_SCHEMA = {
+        "$schema": "http://json-schema.org/draft-07/schema#",
+        "type": "object",
+        "title": "GCP gcs client config.",
+        "default": None,
+        "additionalProperties": False,
+        "properties": {
+            "project": {
+                "type": "string",
+                "description": "The project which the client acts on behalf of."
+            },
+            "credentials": {
+                "type": "object",
+                "description": "The OAuth2 Credentials to use for this client.",
+                "default": None,
+                "additionalProperties": False,
+                "properties": {
+                    "token": {
+                        "type": "string",
+                        "description": "The bearer token that can be used in HTTP headers to make authenticated requests."
+                    },
+                    "expired": {
+                        "type": "boolean",
+                        "description": "Checks if the credentials are expired."
+                    },
+                    "valid": {
+                        "type": "boolean",
+                        "description": "Checks the validity of the credentials."
+                    },
+                    "expiry": {
+                        "type": "string",
+                        "format": "date-time",
+                        "description": "When the token expires and is no longer valid."
+                    },
+                },
+            },
+            "client_info": {
+                "type": "object",
+                "description": "The client info used to send a user-agent string with requests.",
+                "default": None,
+                "additionalProperties": False,
+                "properties": {
+                    "python_version": {
+                        "type": "string",
+                        "description": "The Python interpreter version."
+                    },
+                    "grpc_version": {
+                        "type": "string",
+                        "description": "The gRPC library version."
+                    },
+                    "api_core_version": {
+                        "type": "string",
+                        "description": "The google-api-core library version."
+                    },
+                    "gapic_version": {
+                        "type": "string",
+                        "description": "The sversion of gapic-generated client library."
+                    },
+                    "client_library_version": {
+                        "type": "string",
+                        "description": "The version of the client library."
+                    },
+                    "user_agent": {
+                        "type": "string",
+                        "description": "Prefix to the user agent header."
+                    }
+                }
+            },
+            "client_options": {
+                "type": "object",
+                "description": "Client options used to set user options on the client.",
+                "default": None,
+                "additionalProperties": False,
+                "properties": {
+                    "api_endpoint": {
+                        "type": "string",
+                        "description": "The desired API endpoint.",
+                        "examples": [
+                            "compute.googleapis.com",
+                        ]
+                    },
+                    "quota_project_id": {
+                        "type": "string",
+                        "description": "A project name that a client’s quota belongs to.",  
+                    },
+                    "credentials_file": {
+                        "type": "string",
+                        "description": "A path to a file storing credentials.",  
+                    },
+                    "scopes": {
+                        "type": "array",
+                        "description": "OAuth access token override scopes.",  
+                        "items": {
+                            "type": "string",    
+                        },
+                        "examples": [
+                            [
+                                "https://www.googleapis.com/auth/devstorage.full_control", 
+                                "https://www.googleapis.com/auth/devstorage.read_only"
+                            ]
+                        ]
+                    }
+                }
+            }
+        }
+    }
+    
     def __init__(self, configuration: dict = None):
         if configuration:
             err = Client._validator(Client.CLIENT_CONFIG_SCHEMA,
                                     configuration)
             if err:
                 raise exceptions.ConfigurationError(Exception(err))
+        
+            if "credentials" in configuration:
+                if configuration['credentials']:
+                    if "expiry" in configuration['credentials']:
+                        configuration['credentials']['expiry'] = time.strptime(
+                            configuration['credentials']['expiry'])
+                    configuration['credentials'] = google.auth.credentials.Credentials(
+                        **configuration['credentials'])
+            
+            if "client_info" in configuration:
+                if configuration['client_info']:
+                    configuration['client_info'] = google.api_core.client_info.ClientInfo(
+                        **configuration['client_info'])
+        
         try:
-            self.client = storage.Client(configuration) if configuration\
+            self.client = storage.Client(**configuration) if configuration\
                 else storage.Client()
         except Exception as ex:
             raise ConnectionError(ex)
@@ -46,7 +169,7 @@ class Client(ClientCommon):
 
         Raises:
           PermissionError: Raised when a ListBuckets operation is not permitted.
-          ConnectionError: Raised when a connection error to s3 occurred.
+          ConnectionError: Raised when a connection error to gcs occurred.
         """
         try:
             return [bucket.id for bucket in list(self.client.list_buckets())]
@@ -68,12 +191,22 @@ class Client(ClientCommon):
           List of tuples with object name and its size in bytes.
 
         Raises:
-          PermissionError: Raised when a ListBuckets operation is not permitted.
+          PermissionError: Raised when a storage.buckets.list operation is not permitted.
           cloud_connectors.cloud_storage.exceptions.BucketNotFound: 
             Raised when the object not found.
         """
-        raise NotImplementedError()
-    
+        try:
+            bucket_obj = self.client.lookup_bucket(bucket)
+            if not bucket_obj:
+                raise exceptions.BucketNotFound(
+                    Exception(f"Bucket '{bucket}' not found."))
+            
+            return [(i.name, int(i._properties['size'])) 
+                    for i in bucket_obj.list_blobs(prefix=prefix, 
+                                                   max_results=max_objects)]
+            
+        except Exception as ex:
+            raise PermissionError(ex)
     
     def read(self,
              bucket: str,
